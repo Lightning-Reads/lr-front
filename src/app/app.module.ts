97bb13e0a4bd5101ce9f194e846d1f8a6f7d6fb6
LucaVazz <luca.vazzano@gmx.net>
2017-10-28 18:31:15 +0200

    added input page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StructuredTextInputComponent } from './structured-text-input/structured-text-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StructuredTextInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/structured-text-input/structured-text-input.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-structured-text-input',
+    templateUrl: './structured-text-input.component.html',
+    styleUrls: ['./structured-text-input.component.css']
+})
+export class StructuredTextInputComponent implements OnInit {
+    wholeText = '';
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
