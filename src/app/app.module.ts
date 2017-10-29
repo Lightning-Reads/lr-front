b20bed9a9c90d6b7261a68dcc620a3bb1143a0d8
LucaVazz <luca.vazzano@gmx.net>
2017-10-29 11:29:52 +0100

    implemented word cloud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule }   from '@angular/forms'
 import { NgModule } from '@angular/core'
 
+import {AgWordCloudModule, AgWordCloudData} from 'angular4-word-cloud';
+
 import { AppComponent } from './app.component'
 import { StructuredTextInputComponent } from './structured-text-input/structured-text-input.component'
 import { LoadAnimationComponent } from './load-animation/load-animation.component'
 import { TextComponent } from './text/text.component'
-import { AnalysisService } from './analysis.service'
+import { AnalysisService } from './analysis.service';
+import { WordCloudComponent } from './word-cloud/word-cloud.component'
 
 
 @NgModule({
@@ -15,10 +18,12 @@ import { AnalysisService } from './analysis.service'
     StructuredTextInputComponent,
     LoadAnimationComponent,
     TextComponent,
+    WordCloudComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AgWordCloudModule.forRoot()
   ],
   providers: [AnalysisService],
   bootstrap: [AppComponent]
